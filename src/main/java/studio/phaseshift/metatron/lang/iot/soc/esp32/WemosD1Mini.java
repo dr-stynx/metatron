550dd96125f4f8f6b88a6d009ac504e2438c5fcc
Marko A. Rodriguez <okrammarko@gmail.com>
2025-12-18 22:09:09 -0700

    added ascii art rendering of wemos d1 mini.

--- src/main/java/studio/phaseshift/metatron/lang/iot/soc/esp32/WemosD1Mini.java
+++ src/main/java/studio/phaseshift/metatron/lang/iot/soc/esp32/WemosD1Mini.java
@@ -31,4 +31,20 @@ public class WemosD1Mini extends MRec implements SoC {
     public WemosD1Mini(final fURI tid, final fURI vid) {
         super(Common.mutableMap(), tid, vid);
     }
+    
+    public String render() {
+        String rendering = """
+            {{b}}+-------------------+
+            {{b}}|{{w}}1  16       23  8 {{b}}| 
+            {{b}}|{{w}}2  17       24  9 {{b}}|
+            {{b}}|{{w}}3  18       25 10 {{b}}|
+            {{b}}|{{w}}4  19       26 11 {{b}}| 
+            {{b}}|{{w}}5  20       27 12 {{b}}|
+            {{b}}|{{w}}6  21       28 13 {{b}}|
+            {{b}}|{{w}}7  22       29 14 {{b}}|
+              {{b}}\\_         {{w}}30 15{{b}}|
+                 {{b}}+____{{y}}USB{{b}}_______+{{X}}
+        """;
+        return rendering;
+    }
 }
