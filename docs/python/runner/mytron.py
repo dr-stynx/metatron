cf3bebddc7b88ba842277a0cdc5b1d0c7e9f2817
Marko A. Rodriguez <okrammarko@gmail.com>
2026-02-17 01:23:21 -0700

    fixed a namespace issue in doc runner.

--- docs/python/runner/mytron.py
+++ docs/python/runner/mytron.py
@@ -36,7 +36,7 @@ class Mytron:
         logger.info(f"connected to {self.host}")
 
     def exec(self, code: str) -> list:
-        doc_call: str = f"\"\"\"{code}\"\"\"./mtron/web/inst/doc()"
+        doc_call: str = f"\"\"\"{code}\"\"\"./m/web/inst/doc()"
         print(f"send: {doc_call}")
         future = asyncio.get_event_loop().run_until_complete(submit(self.ws, f"{doc_call}"))
         result = str(future, 'utf-8').strip()
