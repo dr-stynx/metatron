8d067e76f53ed97ee45eb9380d0905ba9a931288
Marko A. Rodriguez <okrammarko@gmail.com>
2026-03-15 17:36:04 -0600

    docs on instruction argument types.

--- src/main/java/studio/phaseshift/metatron/isa/grph/tp3/space/tp3Space.java
+++ src/main/java/studio/phaseshift/metatron/isa/grph/tp3/space/tp3Space.java
@@ -164,7 +164,8 @@ public class tp3Space extends grphSpace<Graph> {
                 return obj;
             Space.Helper.resolveWrite(LOG, this, vid.basePath(), obj, this.directWriter(), this.directReader());
             //return obj;
-            return studio.phaseshift.metatron.furi.Q.Helper.processPostWrite(this.qs(), vid, vid, obj).orElse(studio.phaseshift.metatron.furi.Q.Helper.processQlessWrite(this.qs(), vid, vid, obj).orElse(obj));
+            return studio.phaseshift.metatron.furi.Q.Helper.processPostWrite(this.qs(), vid, vid, obj)
+                    .orElse(studio.phaseshift.metatron.furi.Q.Helper.processQlessWrite(this.qs(), vid, vid, obj).orElse(obj));
         });
     }
 
